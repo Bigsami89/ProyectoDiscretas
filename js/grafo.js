a4dc1cdcfe7b6d3565a4409f24c888dca089976b
Samuel <A23216402@alumnos.uady.mx>
2026-05-14 08:20:14 -0600

    Refinement: Corrected incidence matrix for self-loops and adjacency matrix for multigraphs

--- js/grafo.js
+++ js/grafo.js
@@ -46,7 +46,7 @@ class Grafo {
      * @returns {number[][]} Matriz cuadrada n x n.
      * Una matriz de adyacencia registra las conexiones entre pares de nodos.
      * Si existe una arista entre el nodo i y el nodo j con peso w, entonces A[i][j] = w.
-     * Como tratamos con grafos no dirigidos, la matriz es simétrica (A[i][j] = A[j][i]).
+     * En caso de multigrafos, conservamos el peso MÍNIMO para favorecer el algoritmo de Prim.
      */
     obtenerMatrizAdyacencia() {
         const n = this.nodos.length;
@@ -55,9 +55,13 @@ class Grafo {
         this.aristas.forEach(arista => {
             const i = this.indiceNodos[arista.origen];
             const j = this.indiceNodos[arista.destino];
-            // Asignar el peso en ambas direcciones para representar no-direccionalidad
-            matriz[i][j] = arista.peso;
-            matriz[j][i] = arista.peso; 
+            
+            const pesoActual = matriz[i][j];
+            // Si es la primera arista (0) o si el nuevo peso es menor, actualizamos
+            if (pesoActual === 0 || arista.peso < pesoActual) {
+                matriz[i][j] = arista.peso;
+                matriz[j][i] = arista.peso; 
+            }
         });
 
         return matriz;
@@ -68,6 +72,7 @@ class Grafo {
      * @returns {number[][]} Matriz de n x m (Nodos x Aristas).
      * Una matriz de incidencia muestra la relación entre los nodos (filas) y las aristas (columnas).
      * Si la arista k conecta los nodos i y j, entonces M[i][k] = 1 y M[j][k] = 1.
+     * En caso de bucles (self-loops), el valor es 2 ya que la arista incide dos veces en el mismo nodo.
      */
     obtenerMatrizIncidencia() {
         const n = this.nodos.length;
@@ -77,9 +82,15 @@ class Grafo {
         this.aristas.forEach((arista, col) => {
             const i = this.indiceNodos[arista.origen];
             const j = this.indiceNodos[arista.destino];
-            // Marcamos con 1 los dos nodos que "tocan" esta arista
-            matriz[i][col] = 1;
-            matriz[j][col] = 1;
+            
+            if (i === j) {
+                // Bucle: la arista incide dos veces en el mismo vértice
+                matriz[i][col] = 2;
+            } else {
+                // Arista normal: incide en dos vértices distintos
+                matriz[i][col] = 1;
+                matriz[j][col] = 1;
+            }
         });
 
         return matriz;
